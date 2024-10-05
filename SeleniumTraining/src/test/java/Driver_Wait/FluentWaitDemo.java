5f6f083d91625837ea2dfca80bfe5d21faebc362
HuyDangTu <huy311099@gmail.com>
2024-10-05 11:59:52 +0700

    Fluent Wait

--- SeleniumTraining/src/test/java/Driver_Wait/FluentWaitDemo.java
+++ SeleniumTraining/src/test/java/Driver_Wait/FluentWaitDemo.java
@@ -1,8 +1,12 @@
 package Driver_Wait;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
@@ -14,15 +18,26 @@ public class FluentWaitDemo {
 		// TODO Auto-generated method stub
 		WebDriver driver =  new EdgeDriver();
 		
+		//declare Fluent wait
+		//In 30s, every 5s wait shall looking for the element
+		// if the element was found, return the element
+		// if the element was not found, after 5s find the element again
 		
 		Wait<WebDriver> myWait = new FluentWait<WebDriver>(driver)
 				.withTimeout(Duration.ofSeconds(30))
 				.pollingEvery(Duration.ofSeconds(10))
-				.withMessage("Selenium");
+				.withMessage("textbox not found")
+				.ignoring(NoSuchElementException.class);
 			
 		driver.get("https://opensource-demo.orangehrmlive.com/auth/login");
-		
 		driver.manage().window().maximize();
+		
+		WebElement txtUserName = myWait.until(new Function<WebDriver, WebElement>(){
+			public WebElement apply(WebDriver driver) {
+				return driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
+			}
+			
+		});
+		txtUserName.sendKeys("Admin");
 	}
-
 }
